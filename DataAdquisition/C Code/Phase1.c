a27143bc8cac38ad6b3d2c5eef795bcd12ee5ae5
educacionbm <educacionbm@gmail.com>
2022-07-25 05:56:40 -0400

    Agregando escalamiento a las mediciones

--- DataAdquisition/C Code/Phase1.c
+++ DataAdquisition/C Code/Phase1.c
@@ -7,36 +7,41 @@
 #USE Standard_IO(C)
 #USE Standard_IO(D)
 
-// Pines Pantalla LCD
-#Define LCD_DB4 PIN_B4
-#Define LCD_DB5 PIN_B5
-#Define LCD_DB6 PIN_B6
-#Define LCD_DB7 PIN_B7
-#Define LCD_RS PIN_B2
-#Define LCD_E PIN_B3
-#Include <LCD_16X2.c> // Libreria LCD
+#Include <LCD420.c> // Libreria LCD
 
 Float s1,s2,s3,s4; // Variables de Sensores
 
 Void Main (){
    LCD_Init();
-   Setup_ADC_Ports(AN0_TO_AN1);
+   Setup_ADC_Ports(AN0_TO_AN3);
    Setup_ADC(ADC_Clock_Internal);
 
    While(1){
       Set_ADC_channel(0);
       Delay_us(10);
-      s1 = Read_ADC();
+      s1 = Read_ADC()*0.55;
       
       Set_ADC_channel(1);
       Delay_us(10);
-      s2 = Read_ADC();
+      s2 = Read_ADC()*0.44;
       
-      LCD_Clear();
+      Set_ADC_channel(2)*0.33;
+      Delay_us(10);
+      s3 = Read_ADC();
+      
+      Set_ADC_channel(3)*0.22;
+      Delay_us(10);
+      s4 = Read_ADC();
+      
+      LCD_Putc("\f");
       LCD_Gotoxy(1,1);
       Printf(LCD_Putc,"ADC: %f",s1);
       LCD_Gotoxy(1,2);
       Printf(LCD_Putc,"ADC: %f",s2);
+       LCD_Gotoxy(1,3);
+      Printf(LCD_Putc,"ADC: %f",s3);
+      LCD_Gotoxy(1,4);
+      Printf(LCD_Putc,"ADC: %f",s4);
       Delay_ms(100);  
    }
 }
